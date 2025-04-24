22857ff94bc665d12c6996a4c37c52bd8c2fb69f
Criss <cr.bustosr@duocuc.cl>
2025-04-24 10:23:14 -0400

    Segundo commit creando más anotaciones

--- src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
+++ src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
@@ -19,4 +19,36 @@ public class HolaMundoController {
     public String enviarSaludo(@RequestBody String saludo) {
         return "Recibido: " + saludo;
         }
+
+    @RequestMapping(value = "/pedro", method = RequestMethod.GET)
+    public String pedro() {
+        return "Hola a todos, mi nombre es Pedro.";
+    }
+
+    @GetMapping("/saludo/{nombre}")
+    public String saludo(@PathVariable String nombre) {
+        return "¡Hola a todos, mi nombre es " + nombre + "!";
+    }
+
+    @GetMapping("/sumar/{a}/{b}")
+    public int sumar(@PathVariable int a, @PathVariable int b) {
+        return a+b;
+    }
+
+    @GetMapping("/restar/{a}/{b}")
+    public int restar(@PathVariable int a, @PathVariable int b) {
+        return a-b;
+    }
+
+    @GetMapping("/multiplicar/{a}/{b}")
+    public int multiplicar(@PathVariable int a, @PathVariable int b) {
+    return a*b;
+    }
+
+    @GetMapping("/dividir/{a}/{b}")
+    public String dividir(@PathVariable int a, @PathVariable int b) {
+        if (b==0) {
+            return "Error, no se puede dividir entre 0";
+        }else {return String.valueOf(a/b);}
+    }
 }
