250771d43772ce4bb33dbd114b1d30cf031b64c9
Criss <cr.bustosr@duocuc.cl>
2025-04-24 09:48:41 -0400

    Segundo commit creando más anotaciones

--- src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
+++ src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
@@ -1,12 +1,22 @@
 package com.holamundo.ejemplo.holamundo.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
+@RequestMapping("/api")
+
 public class HolaMundoController {
+
     @GetMapping("/hola")
     public String holaMundo() {
-        return "Gigoló" +
-                "\n\tGigolá" +
-                "\nGingolo";
+        return "Gigoló";
     }
+
+    @RequestMapping(value = "/saludo", method = RequestMethod.GET)
+    public String saludo() {
+        return "Hola Mundo";
+    }
+    @RequestMapping(value = "/saludo", method = RequestMethod.POST)
+    public String enviarSaludo(@RequestBody String saludo) {
+        return "Recibido: " + saludo;
+        }
 }
